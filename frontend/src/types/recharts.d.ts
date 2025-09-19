4f22a26f552917415caf4e38cac97d76ec66464a
binyaminsemerci-ops <binyaminsemerci@gmail.comgit push --set-upstream origin mainit remote set-url origin https://github.com/binyaminsemerci-ops/quantum_trader.gitgit config --global user.name>
2025-09-19 06:44:10 +0200

    chore(ts): add migration plan, re-export helper, shared types, CI and husky hooks

---
+++ frontend/src/types/recharts.d.ts
@@ -0,0 +1,18 @@
+declare module 'recharts' {
+  import * as React from 'react';
+  export const ResponsiveContainer: React.ComponentType<any>;
+  export const LineChart: React.ComponentType<any>;
+  export const ComposedChart: React.ComponentType<any>;
+  export const CartesianGrid: React.ComponentType<any>;
+  export const XAxis: React.ComponentType<any>;
+  export const YAxis: React.ComponentType<any>;
+  export const Tooltip: React.ComponentType<any>;
+  export const Line: React.ComponentType<any>;
+  export const Bar: React.ComponentType<any>;
+  export const Rectangle: React.ComponentType<any>;
+  export const Area: React.ComponentType<any>;
+  export const Legend: React.ComponentType<any>;
+  export const Brush: React.ComponentType<any>;
+  export const BarChart: React.ComponentType<any>;
+  export default {} as any;
+}
