95de25e044a6a73da9231464cc579aadc042073d
binyaminsemerci-ops <binyaminsemerci@gmail.comgit push --set-upstream origin mainit remote set-url origin https://github.com/binyaminsemerci-ops/quantum_trader.gitgit config --global user.name>
2025-09-26 02:51:28 +0200

    chore(codemod): run auto_fix_mypy (bot)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { DashboardProvider } from './hooks/useDashboardData';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
-      <App />
+      <DashboardProvider>
+        <App />
+      </DashboardProvider>
     </React.StrictMode>
   );
 }
